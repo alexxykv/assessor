1409af98ce46d1dae7dcc4795ce2acda432603a9
alexxykv <alexxonov@gmail.com>
2021-06-20 21:19:02 +0500

    Add Codeforces

---
+++ parsing/codeforces/codeforces.py
@@ -0,0 +1,100 @@
+import requests
+import json
+
+
+class Codeforce:
+    """
+    Class for accessing the Codeforce API.
+    """
+
+    API = 'https://codeforces.com/api/'
+    USER_AGENT = ('Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) ' +
+                 'AppleWebKit/537.36 (KHTML, like Gecko) ' +
+                 'Chrome/39.0.2171.95 Safari/537.36')
+
+    def __init__(self):
+        """Initialization"""
+        
+        self.session = requests.Session();
+        self.session.headers = {
+            'User-Agent': Codeforce.USER_AGENT,
+        }
+
+    def _get(self, method_name: str, params: dict):
+        """
+        GET request to codeforces API.
+
+        `method_name`: Method API name
+        `params`: params
+        """
+
+        req = Codeforce.API + method_name
+        res = self.session.get(req, params=params)
+
+        dict = json.loads(res.text)
+        status = dict['status']
+
+        if status == 'OK':
+            result = dict['result']
+            return result
+        elif status == 'FAILED':
+            comment = dict['comment']
+            raise HandleNotFound(comment)
+
+    def user_info(self, handles: list):
+        """
+        Returns information about one or more users.
+
+        `handles`: A list of handles. You can transfer up to 10,000 handles.
+        """
+
+        method_name = 'user.info'
+        params = {
+            'handles': ';'.join(handles),
+        }
+
+        try:
+            res = self._get(method_name, params)
+            return res
+        except HandleNotFound:
+            return []
+
+    def user_blog_entries(self, handle: str):
+        """
+        Returns a list of the specified user's blog entries.
+
+        `handle`: Codeforces user handle.
+        """
+
+        method_name = 'user.blogEntries'
+        params = {
+            'handle': handle,
+        }
+
+        try:
+            res = self._get(method_name, params)
+            return res
+        except HandleNotFound:
+            return []
+
+    def user_rating(self, handle: str):
+        """
+        Returns the rating history of the specified user.
+
+        `handle`: Codeforces user handle.
+        """
+
+        method_name = 'user.rating'
+        params = {
+            'handle': handle,
+        }
+
+        try:
+            res = self._get(method_name, params)
+            return res
+        except HandleNotFound:
+            return []
+
+
+class HandleNotFound(Exception):
+    pass
